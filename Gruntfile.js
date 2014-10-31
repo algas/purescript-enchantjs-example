13ae4cd098e72580ba59618973687e95786c0a70
Masahiro Yamauchi <yamauchi@geishatokyo.com>
2014-10-31 12:02:29 +0900

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({
+
+    libFiles: [
+      "src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs"
+    ],
+
+    psc: {
+      options: {
+        main: "Main",
+        modules: ["Main"]
+      },
+      all: {
+        src: ["<%=libFiles%>"],
+        dest: "js/index.js"
+      }
+    },
+
+    dotPsci: ["<%=libFiles%>"]
+
+  });
+
+  grunt.loadNpmTasks("grunt-purescript");
+
+  grunt.registerTask("main", ["psc:all"]);
+  grunt.registerTask("default", ["main", "dotPsci"]);
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# purescript-enchantjs-example
+
+Example application of [purescript-enchantjs](https://github.com/algas/purescript-enchantjs)
+
+## Usage
+
+Run the following commands.
+
+```
+npm install
+bower update
+grunt
+wget -O js/enchant.js https://raw.githubusercontent.com/uei/enchant.js-builds/master/build/enchant.js
+wget https://raw.githubusercontent.com/wise9/enchant.js/master/images/chara1.png
+open index.html
+```
